5e68a9d7dccb161a6807a5065e836f7904893b72
David Eddy <me@davidjeddy.com>
2026-04-19 02:23:51 +0200

    chore: archive project — history cleared, redirecting to Codeberg
    This repository has been archived. The project has moved to:
    https://codeberg.org/DavidJEddy/yii2-leaflet-draw-plugin

---
+++ src/Draw.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * @copyright Copyright (c) 2015 David J Eddy
+ * @link http://davidjeddy.com
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+namespace davidjeddy\leaflet\plugins\draw;
+
+
+use davidjeddy\leaflet\Plugin;
+use yii\web\JsExpression;
+use yii\helpers\Json;
+
+/**
+ * @author David J Eddy <me@davidjeddy.com>
+ * @link http://www.davidjeddy.com/
+ * @link https://github.com/davidjeddy
+ * @package davidjeddy\leaflet\plugins\draw
+ */
+class Draw extends Plugin
+{
+    /**
+     * @var string the icon name
+     * @see https://github.com/lvoogdt/Leaflet.draw#properties
+     */
+    public $icon;
+
+    /**
+     * Generates the code to generate a maki marker. Helper method made for speed purposes.
+     *
+     * @param string $icon the icon name
+     * @param array $options the maki marker icon
+     *
+     * @return string the resulting js code
+     */
+    public function make($icon, $options = [])
+    {
+        $options['icon'] = $icon;
+        $options = Json::encode($options);
+        return new JsExpression("L.Draws.icon($options)");
+    }
+
+    /**
+     * Returns the plugin name
+     * @return string
+     */
+    public function getPluginName()
+    {
+        return 'plugin:draw';
+    }
+
+    /**
+     * Registers plugin asset bundle
+     *
+     * @param \yii\web\View $view
+     *
+     * @return mixed
+     * @codeCoverageIgnore
+     */
+    public function registerAssetBundle($view)
+    {
+        DrawAsset::register($view);
+        return $this;
+    }
+
+    /**
+     * Returns the javascript ready code for the object to render
+     * @return \yii\web\JsExpression
+     */
+    public function encode()
+    {
+        $icon = $this->icon;
+
+        if (empty($icon)) {
+            return "";
+        }
+        $this->clientOptions['icon'] = $icon;
+        $options = $this->getOptions();
+        $name = $this->getName();
+
+        $js = "L.Draws.icon($options)";
+
+        if (!empty($name)) {
+            $js = "var $name = $js;";
+        }
+
+        return new JsExpression($js);
+    }
+
+}
---
+++ src/DrawAsset.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @copyright Copyright (c) 2015 David J Eddy
+ * @link http://davidjeddy.com
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+namespace davidjeddy\leaflet\plugins\draw;
+
+use yii\web\AssetBundle;
+
+/**
+ * DrawAsset
+ *
+ * @author David J Eddy <me@davidjeddy.com>
+ * @link http://www.davidjeddy.com/
+ * @link https://github.com/davidjeddy
+ * @package davidjeddy\leaflet\plugins\draw
+ */
+class DrawAsset extends AssetBundle
+{
+	public $css        = ['leaflet.draw.css'];
+	public $depends    = ['dosamigos\leaflet\LeafLetAsset',];
+	public $js         = ['leaflet.draw.js'];
+	public $sourcePath = '@vendor/davidjeddy/yii2-leaflet-draw-plugin/jslib/dist/';
+
+    public function init()
+    {
+    	// todo When in debug mode load the source files of Draw - DJE - 2015-09-23
+    }
+}
